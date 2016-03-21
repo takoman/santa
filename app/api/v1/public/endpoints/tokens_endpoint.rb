490129fc18062f5e2b2afff4f3d745c5b521af37
Chung-Yi Chi <chung-yi@artsymail.com>
2016-03-21 00:21:29 -0400

    Add /tokens/access_token endpoint to create access tokens with credentials.

--- app/api/v1/public/endpoints/tokens_endpoint.rb
+++ app/api/v1/public/endpoints/tokens_endpoint.rb
@@ -24,6 +24,25 @@ module V1
               expires_at: DateTime.parse(xapp['expires_in'])
             ), with: V1::Public::Presenters::XappTokenPresenter
           end
+
+          # POST /tokens/access_token
+          desc 'Create a new access token.'
+          params do
+            requires :client_id, type: String, desc: 'Client id.'
+            requires :client_secret, type: String, desc: 'Client secret.'
+            requires :grant_type, type: String, desc: 'Grant type to exchange for an access token.'
+            optional :email, type: String, desc: 'Email of the user.'
+            optional :password, type: String, desc: 'Password of the user.'
+            all_or_none_of :email, :password
+          end
+          post :access_token do
+            token = AccessTokenCreatorService.new.create_access_token permitted_params
+            present OpenStruct.new(
+              type: 'access_token',
+              token: token[:access_token],
+              expires_at: token[:expires_at]
+            ), with: V1::Public::Presenters::AccessTokenPresenter
+          end
         end
       end
     end
